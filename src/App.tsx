ce1edabe2f143973a61a41614b2e41c0c39f9a76
Mark Johnson <wmdmark@gmail.com>
2023-01-29 14:23:58 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Heading, VStack } from "@chakra-ui/react"
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <VStack w="full" h="full" minH={"100vh"} spacing={4}>
+      <Heading>Chakra UI</Heading>
+    </VStack>
+  )
 }
 
-export default App;
+export default App
